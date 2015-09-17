f87a55de014df1a58ff89a25fcc1869dc8b3a6d2
leo <facenord.sud@gmail.com>
2015-09-17 09:54:23 +0200

    add a comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  before_action :set_article, only: [:create, :destroy]
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.article = @article
+    if @comment.save
+      redirect_to article_path(@article)
+    else
+      render 'articles/show'
+    end
+  end
+
+  def destroy
+    @comment  = Comment.find params[:id]
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
+  private
+  def set_article_and_comment
+    @article = Article.find(params[:article_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ActiveRecord::Base
+  has_many :comments
+  validates :title, presence: true,
+            length: { minimum: 3 }
+  validates :text, length: { minimum: 5 }, presence: true
+
 end
